9a85f10b0c28bc435210f64d4dd643258379f4f4
littleblack111 <littleblack11111@gmail.com>
2025-09-10 20:27:57 +0800

    enter

--- helix-term/src/ui/picker.rs
+++ helix-term/src/ui/picker.rs
@@ -1089,7 +1089,7 @@ impl<I: 'static + Send + Sync, D: 'static + Send + Sync> Component for Picker<I,
                     (self.callback_fn)(ctx, option, self.default_action);
                 }
             }
-            key!(Enter) => {
+			ctrl!('y') => {
                 // If the prompt has a history completion and is empty, use enter to accept
                 // that completion
                 if let Some(completion) = self
